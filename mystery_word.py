eca1b6ea86323e9f6d5e76195ca48408c2d4f7f0
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-06 13:48:50 -0400

    added some comments uwu

--- mystery_word.py
+++ mystery_word.py
@@ -1,3 +1,5 @@
+# the below chunk of text will strip the new lines from words.txt, import them into a list, and then use random to pull a random word from said list
+
 word_list = [line.rstrip('\n') for line in open("words.txt")]
 
 import random
